c37b3b6856970345ddc3dd072b156492b88fb19c
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-13 10:50:58 -0800

    Change some javascript, mostly the same as before
    Co-Authored-by: Kerry Lang <klang812@gmail.com>
    co-Authored-by: Elena Talanova <lena.talanova.ua@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
-  $("#formOne").submit(function() {
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput = $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
+
+
+
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
