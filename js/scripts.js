d46d53d6b08b2f17807191c359789f234962ad84
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-12 19:37:28 -0800

    Add basic file and html structure, add jQuery to form

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    $(".person1").append();
+    $(".person2").append();
+    $(".animal").append();
+    $(".exclamation").append();
+    $(".verb").append();
+    $(".noun").append();
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
