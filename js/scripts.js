238cc0a731c682debb9337d23d69605d1ce49482
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-12 19:47:39 -0800

    Add more jQuery to grab values from form, change .append to .text

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    $(".person1").append();
-    $(".person2").append();
-    $(".animal").append();
-    $(".exclamation").append();
-    $(".verb").append();
-    $(".noun").append();
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput = $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
     $("#story").show();
 
     event.preventDefault();
   });
 });
+
